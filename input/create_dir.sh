b00600ee771ffc9719ccda32b3de5b7b06d2ecc8
Keylha Silva <keylha.rego@gmail.com>
2023-05-26 19:44:55 +0000

    realizar o download de todos os arquivos

---
+++ input/create_dir.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /input/dados
+
+mkdir dia01
+mkdir dia02
+mkdir dia03
--- input/curso_minsait/scripts/pre_process/download.sh
+++ input/curso_minsait/scripts/pre_process/download.sh
@@ -1,5 +1,18 @@
 
-cd ../../raw
-mkdir categoria
-cd categoria
-curl -O https://raw.githubusercontent.com/caiuafranca/dados_curso/main/categoria.csv
+#!/bin/bash
+
+curl "https://api.github.com/repos/caiuafranca/dados_curso/contents/"|jq -r '.[].name' > ../../../files_list
+
+cat ../../../files_list | while read line
+do
+    nome="$(cut -d'.' -f 1 <<< $line)"
+    echo $nome
+    url_line="${nome%$cr}"
+    if [ $(cut -d'.' -f 2 <<< $line) == "csv" ] 
+    then
+        mkdir $nome
+        cd "$nome"
+        curl -O "https://raw.githubusercontent.com/caiuafranca/dados_curso/main/$url_line.csv"
+        cd ../
+    fi
+done
